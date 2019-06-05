b8ddced6aafad43efedaae4eb0c978ed708e008f
Ashwitha <ashwitha.b.aux@go-jek.com>
2019-06-05 20:49:48 +0530

    Create sensorList queue

--- src/distributed/sensor/sensor.go
+++ src/distributed/sensor/sensor.go
@@ -15,55 +15,70 @@ import (
 	"distributed-example/src/distributed/utils"
 )
 
-var URL = "amqp://guest:guest@localhost:5672"
-var name = flag.String("name", "sensor", "name of the sensor")
-var freq = flag.Uint("freq", 5, "update frequency in cycles/sec")
-var maximumValue = flag.Float64("max", 5., "maximum value for generated readings")
-var minimumValue = flag.Float64("min", 1., "minimum value for generated readings")
+var BrokerInputListenerURL = "amqp://guest:guest@localhost:5672"
+
+//Read parameters from command line
+var name = flag.String("name", "sensor", "name of the sensor.(Making it as the routing key. So it should be unique)")
+var frequency = flag.Uint("frequency", 5, "update frequency in cycles/sec. (How many datapoints to be generated per second)")
+
+//max and min limit for measurement change
+var maximumValue = flag.Float64("maximum", 5., "maximum value for generated readings")
+var minimumValue = flag.Float64("minimum", 1., "minimum value for generated readings")
 var stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement")
+
+//Generate data points
 var randomNumber = rand.New(rand.NewSource(time.Now().UnixNano()))
 var value = randomNumber.Float64()*(*maximumValue-*minimumValue) + *minimumValue
 var nominalValueOfSensor = (*maximumValue-*minimumValue)/2 + *minimumValue
 
 func main() {
-	flag.Parse() //parse command line flags
+	flag.Parse()
 
-	connection, channel := utils.GetChannel(URL)
+	connection, channel := utils.GetChannel(BrokerInputListenerURL)
 	defer connection.Close()
 	defer channel.Close()
 
+	//Uses direct route so only one consumer will receive the data.
 	dataQueue := utils.GetQueue(*name, channel)
+
+	//Pubish creation of new queue to keep track of data routes(sensors)
+	sensorQueue := utils.GetQueue(utils.SensorListQueue, channel)
+	msg := amqp.Publishing{Body: []byte(*name)}
+	channel.Publish("", sensorQueue.Name, false, false, msg)
+
 	signals := getSignals()
 	buffer := new(bytes.Buffer)
 	encoder := gob.NewEncoder(buffer)
 
 	for range signals {
-		calculateValues()
-		reading := dataTransferObject.SensorMessage{
-			Name:      *name,
-			Value:     value,
-			Timestamp: time.Now(),
-		}
-
 		buffer.Reset()
-		encoder.Encode(reading)
-
-		msg := amqp.Publishing{
-			Body: buffer.Bytes(),
-		}
-
-		channel.Publish("", dataQueue.Name, false, false, msg)
-		log.Printf("Reading sent. value: %v", value)
+		publishMessage(encoder, buffer, channel, dataQueue)
 	}
 }
 
+func publishMessage(encoder *gob.Encoder, buffer *bytes.Buffer, channel *amqp.Channel, dataQueue *amqp.Queue) {
+	calculateValues()
+
+	reading := dataTransferObject.SensorMessage{Name: *name, Value: value, Timestamp: time.Now(),}
+	encoder.Encode(reading)
+	msg := amqp.Publishing{Body: buffer.Bytes()}
+	channel.Publish("", dataQueue.Name, false, false, msg)
+
+	log.Printf("Reading sent. value: %v", value)
+}
+
+//Get signal in given frequency.
 func getSignals() <-chan time.Time {
-	numberOfmsPerCycle := strconv.Itoa(1000 / int(*freq)) // 5 cycles/sec => 200 ms/cycle
+	//duration describes the time time between each signal
+	numberOfmsPerCycle := strconv.Itoa(1000 / int(*frequency)) // 5 cycles/sec => 200 ms/cycle
 	duration, _ := time.ParseDuration(numberOfmsPerCycle + "ms")
+
+	//Create channel that triggers at regular intervals.
 	signal := time.Tick(duration)
 	return signal
 }
 
+//Some random logic to generate the values ti vary in time.
 func calculateValues() {
 	var maxStep, minStep float64
 
